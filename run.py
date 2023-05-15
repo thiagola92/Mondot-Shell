e48795c790e18a0d21bc9232a1e8b6622ffe66c8
thiagola92 <thiagola92@gmail.com>
2023-05-15 07:38:54 -0300

    Rename command arguments
    "--input" >> "--code-path"
    "--temporary-file" >> "--tmp-path"
    
    Avoiding confusion between code file and input file.
    Input file is the file which MondotShell expect inputs from Mondot.
    Code file is the starting query code to be executed.
    
    Renaming temporary argument to be more close to the --code-path argument.

--- run.py
+++ run.py
@@ -7,32 +7,32 @@ from mondot.shell import Shell
 
 # fmt: off
 parser = argparse.ArgumentParser(description="Description to be used on mondot")
-parser.add_argument("--input", dest="in_filepath", type=str)
+parser.add_argument("--code-path", dest="code_path", type=str)
 parser.add_argument("--uri", dest="uri", default=["mongodb://127.0.0.1:27017"], action="append", type=str)
 parser.add_argument("--database", dest="db", default=["admin"], action="append", type=str)
 parser.add_argument("--page-size", dest="page_size", default="20", type=int)
-parser.add_argument("--temporary-file", dest="tmp_filepath", default="tmp.py", type=str)
+parser.add_argument("--tmp-path", dest="tmp_path", default="tmp.py", type=str)
 # fmt: on
 
 args = parser.parse_args()
 args = vars(args)
 
-in_filepath: Path = Path(args["in_filepath"]).resolve()  # Absolute path
-tmp_filepath: Path = Path(args["tmp_filepath"]).resolve()  # Absolute path
+code_path: Path = Path(args["code_path"]).resolve()  # Absolute path
+tmp_path: Path = Path(args["tmp_path"]).resolve()  # Absolute path
 
-rewrite_user_code(input_path=in_filepath, output_path=tmp_filepath)
+rewrite_user_code(input_path=code_path, output_path=tmp_path)
 
 # Import temporary code
-spec = importlib.util.spec_from_file_location("temporary_code", str(tmp_filepath))
+spec = importlib.util.spec_from_file_location("temporary_code", str(tmp_path))
 module = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(module)
 
 # Remove temporary code because it's already loaded
-tmp_filepath.unlink(missing_ok=True)
+tmp_path.unlink(missing_ok=True)
 
 Shell(
     uris=args["uri"],
     dbs=args["db"],
-    filepath=str(in_filepath),
+    filepath=str(code_path),
     page_size=args["page_size"],
 ).run(module.code)
